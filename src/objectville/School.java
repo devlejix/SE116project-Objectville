ca2317d07471af143061010d0c54d954dadf7ca6
lejix <lejixiletisim@gmail.com>
2026-06-05 16:14:23 +0300

    Commit 6 Berkay : School , ServiceBuilding

--- src/objectville/School.java
+++ src/objectville/School.java
@@ -1,4 +1,9 @@
 package objectville;
 
-public class School {
+public class School extends ServiceBuilding {
+    public School(int row, int col) { super(row, col, 4); }
+
+    @Override public void applyServiceTo(Zone zone) { zone.giveEducation(); }
+    @Override public boolean appliesToZone(Zone zone) { return zone instanceof HousingZone; }
+    @Override public String getServiceName() { return "education"; }
 }
--- src/objectville/ServiceBuilding.java
+++ src/objectville/ServiceBuilding.java
@@ -1,4 +1,23 @@
 package objectville;
 
-public class ServiceBuilding {
+// Berkay
+public abstract class ServiceBuilding extends Building {
+
+    protected int radius;
+
+    public ServiceBuilding(int row, int col, int radius) {
+        super(row, col);
+        this.radius = radius;
+    }
+
+    public boolean isInRange(int targetRow, int targetCol) {
+        double distance = Math.sqrt(
+                Math.pow(targetRow - this.row, 2) + Math.pow(targetCol - this.col, 2)
+        );
+        return distance <= radius;
+    }
+
+    public abstract void applyServiceTo(Zone zone);
+    public abstract boolean appliesToZone(Zone zone);
+    public abstract String getServiceName();
 }
